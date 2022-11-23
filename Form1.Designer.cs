9d473629eed1e268acd628c630662db117a42677
OmerFraukTekin <114693702+OmerFraukTekin@users.noreply.github.com>
2022-11-23 20:13:46 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbxBaslangic = new System.Windows.Forms.TextBox();
+            this.txbx2Bolen = new System.Windows.Forms.TextBox();
+            this.txbx1Bolen = new System.Windows.Forms.TextBox();
+            this.txbxBitis = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txbxBaslangic
+            // 
+            this.txbxBaslangic.Location = new System.Drawing.Point(225, 224);
+            this.txbxBaslangic.Name = "txbxBaslangic";
+            this.txbxBaslangic.Size = new System.Drawing.Size(74, 20);
+            this.txbxBaslangic.TabIndex = 0;
+            // 
+            // txbx2Bolen
+            // 
+            this.txbx2Bolen.Location = new System.Drawing.Point(225, 477);
+            this.txbx2Bolen.Name = "txbx2Bolen";
+            this.txbx2Bolen.Size = new System.Drawing.Size(74, 20);
+            this.txbx2Bolen.TabIndex = 1;
+            // 
+            // txbx1Bolen
+            // 
+            this.txbx1Bolen.Location = new System.Drawing.Point(225, 432);
+            this.txbx1Bolen.Name = "txbx1Bolen";
+            this.txbx1Bolen.Size = new System.Drawing.Size(74, 20);
+            this.txbx1Bolen.TabIndex = 2;
+            // 
+            // txbxBitis
+            // 
+            this.txbxBitis.Location = new System.Drawing.Point(225, 280);
+            this.txbxBitis.Name = "txbxBitis";
+            this.txbxBitis.Size = new System.Drawing.Size(74, 20);
+            this.txbxBitis.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(354, 71);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(231, 381);
+            this.listBox1.TabIndex = 4;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(896, 71);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(231, 381);
+            this.listBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(167, 231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "başlangıç";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(174, 287);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "bitiş";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 484);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "2. bölen";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(174, 439);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "1.bölen";
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(682, 497);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(167, 31);
+            this.btnHesapla.TabIndex = 10;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.ForeColor = System.Drawing.Color.Red;
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(618, 71);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(231, 381);
+            this.listBox3.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(351, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "1.sayının bölenleri";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(615, 55);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(117, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "2 sayının ortak bölenleri";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(893, 55);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(90, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "2.sayının bölenleri";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1198, 621);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txbxBitis);
+            this.Controls.Add(this.txbx1Bolen);
+            this.Controls.Add(this.txbx2Bolen);
+            this.Controls.Add(this.txbxBaslangic);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbxBaslangic;
+        private System.Windows.Forms.TextBox txbx2Bolen;
+        private System.Windows.Forms.TextBox txbx1Bolen;
+        private System.Windows.Forms.TextBox txbxBitis;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            int baslangic ,bitis, bolen1, bolen2;
+            baslangic = Convert.ToInt32(txbxBaslangic.Text);
+            bitis = Convert.ToInt32(txbxBitis.Text);
+            bolen1 = Convert.ToInt32(txbx1Bolen.Text);
+            bolen2 = Convert.ToInt32(txbx2Bolen.Text);
+            for (int i = baslangic; i <= bitis; i++)
+            {
+                if (i % bolen1 == 0)
+                {
+                    listBox1.Items.Add(i);
+                }
+                if (i % bolen2 == 0)
+                {
+                    listBox2.Items.Add(i);
+                }
+                if(i % bolen1 == 0 && i% bolen2== 0)
+                {
+
+                    listBox3.Items.Add(i);                   
+
+                }
+            }
+        }
+
+       
+    }
+}
